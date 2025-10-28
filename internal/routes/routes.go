e89060d7e4a7604233ac340aaedb3de60e37cffd
alire-alavi <alirezaalavirazavi@hotmail.com>
2025-10-28 19:28:07 +0330

    delete workout route

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -11,5 +11,6 @@ func SetUpRoutes(app *app.Application) *chi.Mux {
 	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
 	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
 	r.Put("/workouts/{id}", app.WorkoutHandler.HandleUpdateWorkoutById)
+	r.Delete("/workouts/{id}", app.WorkoutHandler.HandleDeleteWorkoutById)
 	return r
 }
